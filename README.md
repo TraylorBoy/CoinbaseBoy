92f261b9ce2aaa0f41234bb00fa2a39ec2ab379b
TraylorBoy <traylorboy@protonmail.com>
2022-12-18 12:15:53 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CoinbaseBoy
-Coinbase api wrapper
+CoinbasePro api wrapper
---
+++ coinbaseboy.py
@@ -0,0 +1,12 @@
+"""CoinbasePro wrapper in Python"""
+
+class CBoy:
+  def __init__(self):
+    pass
+
+  def price(of):
+    """Retrieves the price of a coin
+
+    of (String) - Pairing to retrieve price of ex. BTC-USD
+    """
+    pass
